3a1557faae4c6c07619977cbe37aa19fa53b972b
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-01-26 22:11:43 -0500

    placed db queries inside server routes

--- server.js
+++ server.js
@@ -17,38 +17,69 @@ const db = new sqlite3.Database('./db/elections.db', err => {
     console.log('Connected to the election database.');
 });
 
-// // Delete a candidate
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
+// Delete a candidate
+app.delete('/api/candidate/:id', (req,res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
 
 // GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+     WHERE id = ?`;
+     const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// // create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1,'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
 //     if (err) {
 //         console.log(err);
 //     }
-//     console.log(row);
+//     console.log(result, this.lastID);
 // });
 
-// create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1,'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result, this.lastID);
+app.get('/api/candidates', (req, res) => {
+    // set SQL query to SQL variable
+    const sql = `SELECT * FROM candidates`;
+    // params is an empty array here because there are no
+    // placeholders in the sql statement
+    const params = [];
+    //returns all data from candidates table
+    db.all(sql, params, (err,rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
 });
-
-// returns all data from candidates table
-// db.all(`SELECT * FROM candidates`, (err,rows) => {
-//     console.log(rows);
-// });
 
 // default response for any other request (not found) catch all
 // NOTE: catch all routes must be placed below all other routes
